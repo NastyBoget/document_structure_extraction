d53c92f1d053e210efd809fe3a99b27609bc5aa1
NastyBoget <bogatenkova.anastasiya@mail.ru>
2019-12-05 13:42:07 +0300

    first simple version

---
+++ pdf2text.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import pytesseract
+import numpy as np
+import cv2
+import os
+import pdf2image as p2i
+
+def pdf2text(path):
+    """
+    path - путь к файлу pdf
+    файл преобразуется в изображение, изображение преобразуется в текст
+    возвращает список строк документа
+    """
+    img = p2i.convert_from_path(path, fmt='png')
+    image = np.array(img[0])
+    preprocess = "thresh"
+    # загрузить образ и преобразовать его в оттенки серого
+    #image = cv2.imread('image.png') # image - numpy-array of pixels
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # проверим, следует ли применять пороговое значение для предварительной обработки изображения
+    if preprocess == "thresh":
+        gray = cv2.threshold(gray, 0, 255,
+            cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    # если нужно медианное размытие, чтобы удалить шум
+    elif preprocess == "blur":
+        gray = cv2.medianBlur(gray, 3)
+    # сохраним временную картинку в оттенках серого, чтобы можно было применить к ней OCR
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, gray)
+    # загрузка изображения в виде объекта image Pillow, применение OCR, а затем удаление временного файла
+    text = pytesseract.image_to_string(Image.open(filename), lang='rus') #to_data
+    os.remove(filename)
+    return text.split(sep='\n')

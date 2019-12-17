09fac5185dea6e577f07dae3ece03b736368a922
NastyBoget <bogatenkova.anastasiya@mail.ru>
2019-12-17 19:07:47 +0300

    work in progress

---
+++ version3/demo.py
@@ -0,0 +1,23 @@
+from pdf2text import pdf2text
+
+print("Введите имя файла:")
+path = input()
+lines = pdf2text(path)
+X = [line for line in lines]
+
+from string2features import string2features
+import pickle as pkl
+clf = pkl.load(open("model.pkl", "rb"))
+y = clf.predict(X)
+
+res = []
+for line, type_ in zip(X, y):
+    res.append({"type": int(type_), "content": line})
+print(*res, sep='\n')
+
+import json
+with open("result.json", "w") as write_file:
+    json.dump(res, write_file, ensure_ascii=False)
+
+
+

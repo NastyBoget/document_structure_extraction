e7aa8db3a635b2a15d1debe5e28b5af913236411
NastyBoget <bogatenkova.anastasiya@mail.ru>
2019-12-24 14:08:19 +0300

    add rule features

--- string2features.py
+++ string2features.py
@@ -18,3 +18,34 @@ class string2features:
             else:
                 first_words.append('')
         return first_words
+
+import re
+RE_LIST = re.compile(r'\d+(\.\d+)*\D') # для отдельного типа списка
+RE_HEADER = re.compile(r'Раздел|Подраздел|Глава|Параграф|Секция|Часть|Статья')
+
+class AddFeatures:
+    def __init__(self):
+        pass
+    def fit(self):
+        pass
+    def transform(self, X):
+        """
+        returns 2 columns: 1-list, 2-header
+        X - list of strings
+        """
+        features = []
+        for line in X:
+            match = RE_LIST.search(line)
+            if match:
+                if match.start() == 0:
+                    features.append([1, 0])
+                    continue
+            match = RE_HEADER.search(line)
+            if match:
+                if match.start() == 0:
+                    features.append([0, 1])
+                    continue
+            features.append([0, 0])
+        return features
+    def fit_transform(self, X, y=None):
+        return self.transform(X)

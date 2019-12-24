2a9deb894b3f2e9af4c13f05bc8f1e2bc85e2239
NastyBoget <bogatenkova.anastasiya@mail.ru>
2019-12-24 15:31:17 +0300

    changes for report

--- demo.py
+++ demo.py
@@ -1,22 +1,17 @@
 from pdf2text import pdf2text
-
-print("Введите имя файла:")
-path = input()
-X = pdf2text(path)
-
-from string2features import string2features
 import pickle as pkl
-clf = pkl.load(open("model.pkl", "rb"))
-y = clf.predict(X)
-
-res = []
-for line, type_ in zip(X, y):
-    res.append({"type": int(type_), "content": line})
-print(*res, sep='\n')
-
+from string2features import string2features, AddFeatures
 import json
-with open("result.json", "w") as write_file:
-    json.dump(res, write_file, ensure_ascii=False)
+    
+def doc2json(path):
+    X = pdf2text(path)
+    clf = pkl.load(open("model.pkl", "rb"))
+    y = clf.predict(X)
+    res = []
+    for line, type_ in zip(X, y):
+        res.append({"type": int(type_), "content": line})
+    return json.dumps(res, ensure_ascii=False).encode('utf8')
 
-
-
+print("введите путь к файлу")
+path = input()
+print(doc2json(path).decode())
